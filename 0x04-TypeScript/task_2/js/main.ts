0d25b7b6860f7289d2f5e7b2d1b128613266652e
Temi Michael <olasehindemichael285@gmail.com>
2025-11-09 14:56:40 +0100

    Task Update : Task_2 folder created and fixed as per requirement

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,77 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+const createEmployee = (salary: number | string): Director | Teacher => {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+};
+
+console.log(createEmployee(200));
+
+console.log(createEmployee(1000));
+
+console.log(createEmployee("$500"));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+const executeWork = (employee: Director | Teacher): string => {
+  // if (employee instanceof Director) {
+  //     return employee.workDirectorTasks();
+  // }
+  // return employee.workTeacherTask();
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+};
+
+
+const teachClass = (Subjects :  "Math" | "History") => {
+    if (Subjects == "Math") {
+        return `Teaching ${Subjects}`;
+    } else if (Subjects == "History") {
+        return `Teaching ${Subjects}`;
+    }
+    return `Unknown Subject: ${Subjects}`;
+}
